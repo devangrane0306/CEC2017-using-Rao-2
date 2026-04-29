2f9a09e3201c37c10dc1240f44e2733bda76c4f0
Lakshy Maheshwari <lakshymaheshwari27@gmail.com>
2026-04-29 22:45:42 +0530

    Update experimental config: MAX_FES=1M, RUNS=30

--- CEC2017/config.py
+++ CEC2017/config.py
@@ -1,9 +1,9 @@
 DIMENSIONS = [2, 10, 20, 30, 50, 100]
 
-POP_SIZE = 50                          # Population size
-MAX_FES = 500_000                      # FE = Iteration × POP_SIZE = 10,000 × 50
-MAX_ITERATIONS = MAX_FES // POP_SIZE   # Iteration column in CSV = 10,000 per run
-RUNS = 10                              # 10 runs × 10,000 iter = 1,00,000 Total Iterations
+POP_SIZE = 50
+MAX_FES = 1000_000
+MAX_ITERATIONS = MAX_FES // POP_SIZE
+RUNS = 30
 
 LOWER_BOUND = -100
 UPPER_BOUND = 100
